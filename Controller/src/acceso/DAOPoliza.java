03a9f63e6cde35fc3c761bdd74d763272b3ff36a
thevenom1215 <chicowii@hotmail.com>
2018-03-11 17:07:53 -0600

    Delete DAOPoliza.java

--- Controller/src/acceso/DAOPoliza.java
+++
@@ -1,8 +0,0 @@
-package acceso;
-import modelos.Poliza;
-public interface DAOPoliza {
-	public void revisar (Poliza poliza) throws Exception;
-	public void registrar(Poliza poliza) throws Exception;
-	public void editar(Poliza poliza) throws Exception;
-	public void eliminar(Poliza poliza) throws Exception;
-}
