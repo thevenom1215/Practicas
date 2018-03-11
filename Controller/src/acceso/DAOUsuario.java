a76f1ecee50da5788ab750b200f436b134ad33b1
thevenom1215 <chicowii@hotmail.com>
2018-03-11 17:08:29 -0600

    Delete DAOUsuario.java

--- Controller/src/acceso/DAOUsuario.java
+++
@@ -1,9 +0,0 @@
-package acceso;
-import modelos.Usuario;
-
-public interface DAOUsuario {
-public void buscar(Usuario usuario) throws Exception;
-public void registrar(Usuario usuario) throws Exception;
-public void editar(Usuario usuario) throws Exception;
-public void eliminar(Usuario usuario) throws Exception;
-}
